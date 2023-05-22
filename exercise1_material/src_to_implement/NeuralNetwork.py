2218743148ccd1cbc729ae4ac88aed76317bd832
TahaHussain121 <tahahussain121b@gmail.com>
2023-05-22 20:21:05 +0200

    --the neural network

--- exercise1_material/src_to_implement/NeuralNetwork.py
+++ exercise1_material/src_to_implement/NeuralNetwork.py
@@ -1,4 +1,41 @@
-
-
+from copy import deepcopy
 class NeuralNetwork:
-    pass
+    def __init__(self, optimizer):
+        self.optimizer = optimizer
+        self.loss = []
+        self.layers = []
+        self.data_layer = None
+        self.loss_layer = None
+
+    def forward(self):
+        input_tensor, label_tensor = self.data_layer.next() #tensoris just fancy word for array?
+        output = input_tensor
+        for layer in self.layers:
+            output = layer.forward(output)
+        return output
+
+    def backward(self, label_tensor):
+        error_tensor = self.loss_layer.backward(label_tensor)
+        for layer in reversed(self.layers):
+            error_tensor = layer.backward(error_tensor)
+
+    def append_layer(self, layer):
+        if layer.trainable:
+            layer.optimizer = deepcopy(self.optimizer)
+        self.layers.append(layer)
+
+    def train(self, iterations):
+        for _ in range(iterations):
+            prediction = self.forward()
+            loss = self.loss_layer.forward(prediction, self.data_layer.label_tensor)
+            self.loss.append(loss)
+            self.backward(self.data_layer.label_tensor)
+            for layer in self.layers:
+                if layer.trainable:
+                    layer.weights = layer.optimizer.calculate_update(layer.weights, layer.gradient_weights)
+
+    def test(self, input_tensor):
+        output = input_tensor
+        for layer in self.layers:
+            output = layer.forward(output)
+        return output
